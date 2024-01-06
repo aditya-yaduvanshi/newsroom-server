4911c105d5b32c4139b44da410f1fab757170597
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-06 18:47:28 +0530

    feat: added other modules imports in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,23 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { PostsModule } from './posts/posts.module';
+import { SchedulerModule } from './scheduler/scheduler.module';
+import { WebscrapingModule } from './webscraping/webscraping.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.DB_URI),
+    ScheduleModule.forRoot(),
     PostsModule,
+    SchedulerModule,
+    WebscrapingModule,
   ],
   controllers: [AppController],
   providers: [AppService],
