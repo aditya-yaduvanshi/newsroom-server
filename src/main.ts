64b08ca5c60aa5419771865b5d7139817016af31
Aditya Yaduvanshi <adityayaduvanshi0001@gmail.com>
2024-01-06 18:43:30 +0530

    feat: handle uncaught exception and unhandled rejection at runtime

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,18 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(
     AppModule,
-    new FastifyAdapter({ logger: true })
+    new FastifyAdapter({ logger: true }),
   );
-  await app.listen(process.env.PORT || 5000, "0.0.0.0");
+  await app.listen(process.env.PORT || 5000, '0.0.0.0');
 }
 bootstrap();
+
+// Handle uncaught exceptions globally
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception:', error);
+});
+
+// Handle unhandled rejection globally
+process.on('unhandledRejection', (error) => {
+  console.error('Unhandled Rejection:', error);
+});
